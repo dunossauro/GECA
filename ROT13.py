d3b1862212864a5367a782d2ec6f7954291ec9af
z4r4tu5tr4 <z4r4tu5tr4@users.noreply.github.com>
2014-12-10 04:11:23 -0200

    Update ROT13.py

--- ROT13.py
+++ ROT13.py
@@ -1,5 +1,27 @@
 #!/bin/python3.4
 
+"""
+Sobre:
+
+	Esse programa criptografa usando o ROT13, ou cifra do Kama-Sutra.
+
+	É o desafio de férias número 1 proposto pelo GECA (Grupo de estudos criptográficos de Americana)
+
+Versão:
+
+	0.1 (09/12/2014)
+
+Desevolvedores:
+	0.1
+		Eduardo F. Mendes (z4r4tustr4)
+		Jacomo Giovanetti
+
+Debug:
+	0.1
+		William Cesar Godoy Lopes (WgL)
+"""
+
+
 frase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
