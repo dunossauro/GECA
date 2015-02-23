ed5b03da3a62e764ce040b53c51d4bca7e83ed24
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-02-23 03:33:40 -0300

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,69 +1,22 @@
-#!/bin/python3.4
+#!/usr/bin/python 3
 
-#imports
+import string
 
-import os, signal
+def cripto(frase, chave):
 
-#Função que executa a Cifra de cesar
-def cripto(entrada,b):
-"""
-Essa parte do código foi baseada nesse link: http://pastebin.com/W6WGW3JE
+	alfabeto = string.ascii_uppercase
+	saida = ""
 
-E apesar de não resolver o problema do corpo finito e sim andar pela tabela ASCII já possível se fazer algo.
-"""
-
-	a=str()
-	for x in entrada:
-		if b == "0":
- 			a +=(chr(ord(x)+3))
-		if b == "1":
-			a +=(chr(ord(x)-3))
+	for letra in frase:
 	
+		if chave is "1":
+			busca = alfabeto.find(letra)+3
 	
+		else:
+			busca = alfabeto.find(letra)-3
+	
+		modulo = busca % 26
+		saida += str(alfabeto[modulo])
 
-	#Saida
-
-	print("\n\n Frase: " + a)
-
+	print(saida)
 	input("\n\nPressione Enter para continuar")
-
-#função do menu inicial do programa
-def menu():
-	#Menu inicial
-	os.system('clear')
-
-	print("\
-Cifra de Cesar\n\
-=== Menu ===\n\
-1. Criptografar\n\
-2. Descriptografar\n\
-	")
-
-
-"""
-A função replace remove os espaços em branco, para a analise de frequencia se tornar um pouco mais "complicada"
-A função Upper está lá porque antigamente eram usados os caracteres maiusculos no texto criptografado
-A variável b representa um chaveador, quando ativo a def cripto() faz descriptografia
-quando inativo criptografia
-"""
-
-	menu = input(":")
-	if menu == "1":
-		entrada = input("Digite a frase a ser criptografada: \b")
-		entrada = entrada.replace(" ","")		
-		entrada = entrada.upper()
-		b = "0"
-		cripto(entrada,b)
-	elif menu == "2":
-		entrada = input("Digite a frase a ser descriptografada: \b")
-		entrada = entrada.upper()
-		entrada = entrada.replace(" ","")
-		b = "1"
-		cripto(entrada,b)
-
-#Úm laço para manter o menu constante
-def main():
-	while True:
-		menu()
-
-main()
