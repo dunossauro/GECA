be85053cd582280875bc11ee511399153532b499
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-03-24 00:36:19 -0300

    Update duaslinhas.py

--- Cerca-da-ferrovia/duaslinhas.py
+++ Cerca-da-ferrovia/duaslinhas.py
@@ -3,8 +3,14 @@
 mensagem = input("Digite a mensagem:  ").upper().replace(" ","")
 linha1 = ""
 linha2 = ""
+saida = ""
 
-for x in range(0, len(mensagem)):
+#"Tratamento"
+if len(mensagem) % 2 is 1:
+	mensagem += " "
+
+#criptografar
+for x in range(len(mensagem)):
 
 	if x % 2 is 0:
 		linha1 += mensagem[x]
@@ -12,3 +18,11 @@ for x in range(0, len(mensagem)):
 		linha2 += mensagem[x]
 
 print("%s%s" % (linha1,linha2))
+
+#Decriptografar
+for x in range(len(linha1)):
+	
+	saida += linha1[x]
+	saida += linha2[x]
+
+print(saida)
