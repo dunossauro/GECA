17303f3d293b8bf40d3825a87e3d7c0b0ab1838d
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-03-24 00:36:51 -0300

    Update maconica.py

--- maconica/maconica.py
+++ maconica/maconica.py
@@ -5,11 +5,20 @@ system("clear")
 alfabeto_limpo = string.ascii_uppercase
 alfabeto_sujo = string.punctuation
 texto_sujo = ""
+texto_limpo = ""
 
 palavra = input("Digite a o texto claro: ").upper()
 
+#Criptografar
 for x in palavra:
 	a = alfabeto_limpo.find(x)
 	texto_sujo += alfabeto_sujo[a]
 
 print(texto_sujo)
+
+#Descriptografar
+for x in texto_sujo:
+	a = alfabeto_sujo.find(x)
+	texto_limpo += alfabeto_limpo[a]
+
+print(texto_limpo)
