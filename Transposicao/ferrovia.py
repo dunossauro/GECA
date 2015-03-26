90b54ca7b37e91ba22e321f3cc5f547f7134743b
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-03-26 18:27:22 -0300

    Update ferrovia.py

--- Transposicao/ferrovia.py
+++ Transposicao/ferrovia.py
@@ -1,12 +1,23 @@
 mensagem = input("Digite a mensagem:  ").upper().replace(" ","")
+chaveador = input("1 Para criptografar, 2 para descriptografar:  ")
 
 saida = ""
+rodada = ""
 x = 0
 
 while x < 2:
 	
-	saida += mensagem[x::linhas]
-	
+	if chaveador is "1":
+		saida += mensagem[x::2]
+	else:
+		rodada += mensagem[x::2]
 	x += 1
+
+if chaveador is not "1":
+	x = 0
+	while x < 2:
+
+		saida += rodada[x::2]
+		x += 1
 
 print(saida)
