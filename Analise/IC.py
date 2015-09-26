3e39f55b22db813e0f2d979316ac4fa5cdb7fb91
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-26 15:12:15 -0300

    Create IC.py

---
+++ Analise/IC.py
@@ -0,0 +1,19 @@
+import sys
+import collections
+
+arquivo = open(sys.argv[1]).read()
+
+arquivo_limpo = [x.upper() for x in arquivo.split() if x.isalpha()]
+
+#FORMULA
+N = len(arquivo_limpo)
+freqs = collections.Counter(arquivo_limpo)
+alfabeto = [chr(x) for x in range(ord("A"),ord("Z")+1)]
+soma = 0.0
+
+for letra in alfabeto:
+	soma += freqs[letra] * (freqs[letra] -1)
+
+IC = soma/(N*(N-1))
+
+print(IC)
